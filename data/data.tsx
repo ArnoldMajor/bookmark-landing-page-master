e9f16fd197221b2c1fcb90fec51623c0cc0d2f35
ArnoldMajor <arnoldmwangakala@gmail.com>
2025-07-22 18:11:55 +0300

    Desktop --complete structure

---
+++ data/data.tsx
@@ -0,0 +1,115 @@
+export const NavLinks = [
+    'features',
+    'pricing',
+    'contact'
+]
+
+export const HeroCopy = [
+    {
+        heading: 'A Simple Bookmark Manager',
+        paragraph: "A clean and simple interface to organize your favourite websites. Open a new browser tab and see your sites load instantly. Try it for free.",
+        btnPrimary: 'Get it on Chrome',
+        btnSecondary: 'Get it on Firefox',
+        image: '/illustration-hero.svg'
+    }
+]
+
+export const FeaturesCopy = [
+    {
+        heading: 'Features',
+        paragraph: 'Our aim is to make it quick and easy for you to access your favourite websites. Your bookmarks sync between your devices so you can access them on the go.'
+    }
+]
+
+export const FeaturesMenu = [
+    'Simple Bookmarking',
+    'Speedy Searching',
+    'Easy Sharing',
+]
+
+export const FeaturesCard = [
+    {
+        menuItem: 'Speedy Searching',
+        title: 'Intelligent search',
+        paragraph: 'Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.',
+        img: '/illustration-features-tab-2.svg'
+    },
+    {
+        menuItem: 'Simple Bookmarking',
+        title: 'Bookmark in one click',
+        paragraph: 'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.',
+        img: '/illustration-features-tab-1.svg'
+    },
+    {
+        menuItem: 'Easy Sharing',
+        title: 'Share your bookmarks',
+        paragraph: 'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.',
+        img: '/illustration-features-tab-3.svg'
+    }
+]
+
+export const DownloadsCopy = [
+    {
+        heading: 'Download the extension',
+        paragraph: "We've got more browsers in the pipeline. Please do let us know if you've got a favourite you'd like us to prioritize."
+    }
+]
+
+export const DownloadsCards = [
+    {
+        title: 'Add to Chrome',
+        version: 'Minimum version 62',
+        icon: '/logo-chrome.svg'
+    },
+    {
+        title: 'Add to Firefox',
+        version: 'Minimum version 55',
+        icon: '/logo-firefox.svg'
+    },
+    {
+        title: 'Add to Opera',
+        version: 'Minimum version 46',
+        icon: '/logo-opera.svg'
+    }
+]
+
+
+export const FaqCopy = [
+    {
+        heading: 'Frequently Asked Questions',
+        paragraph: "Here are some of our FAQs. If you have any other questions you’d like answered please feel free to email us."
+    }
+]
+
+export const FaqAccordion = [
+    {
+        question: 'What is Bookmark?',
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt justo eget ultricies fringilla. " +
+            "Phasellus blandit ipsum quis quam ornare mattis."
+    },
+    {
+        question: 'How can I request a new browser?',
+        answer: "Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. " +
+            "Suspendisse imperdiet. Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, " +
+            "ultricies non ligula. Suspendisse imperdie tVivamus luctus eros aliquet convallis ultricies. " +
+            "Mauris augue massa, ultricies non ligula. Suspendisse imperdiet."
+    },
+    {
+        question: 'Is there a mobile app?',
+        answer: "Sed consectetur quam id neque fermentum accumsan. Praesent luctus vestibulum dolor, ut condimentum " +
+            "urna vulputate eget. Cras in ligula quis est pharetra mattis sit amet pharetra purus. Sed " +
+            "sollicitudin ex et ultricies bibendum."
+    },
+    {
+        question: 'What about other Chromium browsers?',
+        answer: "Integer condimentum ipsum id imperdiet finibus. Vivamus in placerat mi, at euismod dui. Aliquam " +
+            "vitae neque eget nisl gravida pellentesque non ut velit."
+    },
+]
+
+export const newsletterCopy = [
+    {
+        stat: '35,000+ already joined',
+        title: 'Stay up-to-date with what we’re doing',
+    }
+]
