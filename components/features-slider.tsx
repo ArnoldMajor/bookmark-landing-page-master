19f884b0568b71205f34417d6f9783f1e5dab18e
ArnoldMajor <arnoldmwangakala@gmail.com>
2025-07-25 18:08:15 +0300

    Desktop Responsiveness --complete

--- components/features-slider.tsx
+++ components/features-slider.tsx
@@ -12,12 +12,12 @@ export default function FeaturesSlider() {
 
     return (
         <div>
-            <ul className="px-5 lg:px-40 flex flex-col lg:flex-row justify-center list-none mb-20 2xl:mb-30">
+            <ul className="px-5 lg:px-40 flex flex-col lg:flex-row justify-center list-none mb-20 lg:mb-30 xl:mb-50">
                 {FeaturesMenu.map((menuItem, index) => (
                     <li key={index} className={`flex-1 first:border-t-1 lg:first:border-t-0 text-center text-lg text-neutral-500 font-medium border-b-1 border-neutral-300
                                                 ${activeMenuItem === menuItem ? 'xl:border-b-4 xl:border-red-400' : ''}`}>
                         <button onClick={() => setActiveMenuItem(menuItem)}
-                            className={`cursor-pointer py-4 w-52 lg:w-64
+                            className={`cursor-pointer py-4 w-52 lg:w-64 hover:text-red-400
                             ${activeMenuItem === menuItem ? 'border-b-4 border-red-400 xl:border-b-0 xl:border-none' : ''}`}>
                             {menuItem}
                         </button>
@@ -30,7 +30,7 @@ export default function FeaturesSlider() {
                         {activeMenuItem === cardItem.menuItem ?
                             <>
                                 <div className={`flex flex-1 justify-center xl-${activeMenuItem === 'Simple Bookmarking' ? 'justify-start' : 'justify-center'} overflow-visible relative 
-                                xl:mr-50 mb-65`}>
+                                 mb-65`}>
                                     <Image
                                         key={cardItem.menuItem}
                                         src={cardItem.img}
@@ -38,18 +38,18 @@ export default function FeaturesSlider() {
                                         width={imageWidth}
                                         height={imageHeight}
                                         className={`absolute ${activeMenuItem === 'Simple Bookmarking'
-                                            ? 'scale-90 sm:scale-80 lg:scale-110 sm:-bottom-65 lg:-bottom-50'
-                                            : '-bottom-70 lg:-bottom-60 scale-80 lg:scale-100'}
+                                            ? 'scale-90 sm:scale-80 lg:scale-110 xl:scale-100 sm:-bottom-65 lg:-bottom-50'
+                                            : '-bottom-70 lg:-bottom-60 scale-80 lg:scale-100 xl:left-15'}
                                               2xl:scale-110 z-10`}
                                     />
                                     <div
                                         className="absolute z-5 w-300 bg-blue-600 rounded-r-full
-                                        -bottom-68 xl:-bottom-25 
-                                        -left-220 
+                                        -bottom-68 xl:-bottom-75
+                                        -left-220 xl:-left-200
                                         h-55 xl:h-75 " />
                                 </div>
                                 <div className="flex-1 flex-col lg:pl-20 items-center justify-center
-                                text-center lg:text-left pr-0 mt-15 lg:mt-20">
+                                text-center lg:text-left pr-0 mt-15 lg:mt-0">
                                     <h2 className="font-semibold text-2xl sm:text-4xl lg:text-3xl text-blue-950 mb-8">{cardItem.title}</h2>
                                     <p className="text-lg sm:text-xl text-neutral-500 mb-8">{cardItem.paragraph}</p>
                                     <div className="hidden lg:flex">
--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -5,18 +5,18 @@ import Image from "next/image"
 
 export default function Hero() {
     return (
-        <div className="px-5 sm:px-20 xl:px-40 2xl:px-80 pt-25 sm:pt-35 md:pt-50">
+        <div className="px-5 sm:px-20 xl:px-40 2xl:px-80 pt-25 sm:pt-35 md:pt-50 xl:pt-65">
             {HeroCopy.map((item, index) => (
                 <div key={index} className="flex flex-col-reverse lg:flex-row pb-40 items-center">
-                    <div className="flex-1 flex-col text-center lg:text-left items-center justify-center">
-                        <h1 className="font-bold text-3xl sm:text-5xl lg:text-4xl text-blue-950 mb-8">{item.heading}</h1>
+                    <div className="flex-1 flex-col text-center lg:text-left items-center justify-center xl:pr-15">
+                        <h1 className="font-bold text-3xl sm:text-5xl lg:text-4xl xl:text-5xl text-blue-950 mb-8">{item.heading}</h1>
                         <p className="text-lg text-neutral-500 mb-8">{item.paragraph}</p>
                         <div className="flex flex-row space-x-6 justify-center lg:justify-start">
                             <Button paddingX="px-4" btnClasses="font-medium" btnColor="bg-blue-600" btnBorder="border-blue-600" btnBorderHover="hover:border-blue-600" textColorHover="hover:text-blue-600" btnLabel={item.btnPrimary} />
                             <Button paddingX="px-4" btnClasses="font-medium" btnColor="bg-neutral-100" textColor="text-blue-950" btnBorder="border-neutral-100" btnBorderHover="hover:border-blue-950" btnLabel={item.btnSecondary} />
                         </div>
                     </div>
-                    <div className="flex-1 overflow-visible relative pb-50 w-100
+                    <div className="flex-1 overflow-visible relative pb-50 xl:pb-0 w-100
                                     mb-30 sm:mb-50 lg:mb-30">
                         <Image
                             src={'/illustration-hero.svg'}
@@ -24,11 +24,11 @@ export default function Hero() {
                             width={500}
                             height={300}
                             className="absolute z-10
-                            scale-100 sm:scale-120 lg:scale-100 2xl:scale-110"
+                            scale-100 sm:scale-120 lg:scale-100 xl:scale-120 xl:-bottom-60"
                         />
                         <div className="absolute z-5 w-300 bg-blue-600 rounded-l-full
-                        -bottom-20 sm:-bottom-30 xl:-bottom-25 
-                        -right-220 
+                        -bottom-20 sm:-bottom-30 xl:-bottom-75 
+                        -right-220 xl:-right-200
                         h-55 sm:h-65 xl:h-75 ">
                         </div>
                     </div>
