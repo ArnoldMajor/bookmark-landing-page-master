a2b5270b498309584aff24d0d78868a78d385d1b
ArnoldMajor <arnoldmwangakala@gmail.com>
2025-08-05 12:43:15 +0300

    Project --complete

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  images: {
-    loader: "akamai", // Or your preferred image loader for static sites
-    path: "",
-  },
-  assetPrefix: "./", // Important for correct asset paths on GitHub Pages
 };
 
 export default nextConfig;
